3d7af8e61fc5446f607991e72ab99790e256e852
Smaranda02 <62556419+Smaranda02@users.noreply.github.com>
2024-02-01 16:56:47 +0200

    Add files via upload

---
+++ EWallet/Controllers/ErrorController.cs
@@ -0,0 +1,35 @@
+﻿using EWallet.WebApp.Code.Base;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
+
+namespace EWallet.Controllers
+{
+    public class ErrorController : BaseController
+    {
+        public ErrorController(ControllerDependencies dependencies) : base(dependencies) 
+        { 
+        }
+
+        public IActionResult Index()
+        {
+            return View("Error");
+        }
+
+        public IActionResult NotFound()
+        {
+            
+            return View();
+
+        }
+
+        public IActionResult Unauthorized()
+        {
+            return View();
+        }
+
+        public IActionResult InternalServerError()
+        {
+            return View();
+        }
+    }
+}
